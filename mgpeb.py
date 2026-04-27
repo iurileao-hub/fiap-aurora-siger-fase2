c5d8346b163d480fd2db127e49a65bd4ce590a25
Gabriel Carmona <191816876+Gcarmnonapy7@users.noreply.github.com>
2026-04-26 22:01:53 -0300

    Adding : Structuring types for modules, atmosfer classifier and condition

--- mgpeb.py
+++ mgpeb.py
@@ -11,14 +11,68 @@ baseadas em lógica booleana, utilizando estruturas de dados lineares,
 algoritmos clássicos de busca e ordenação, e funções matemáticas aplicadas.
 """
 
-import math
-
+from collections import deque
+from dataclasses import dataclass
+from typing import Optional, List, Tuple, Any
+from enum import Enum
+import time
+import random
 
 # =============================================================================
-# [1] DADOS — Módulos da colônia e condições simuladas
+# [1] Data - Classes struct
 # =============================================================================
 
+class ModuleType(Enum):
+    HABITAT = "habitation"
+    ENERGY = "energy"
+    LAB = "laboratory"
+    LOGISTIC = "Logistic"
+    MEDICAL = "Medical support"
+    
+class AtmosfericClassifier(Enum):
+    GREEN = "OK"
+    YELLOW = "CAUTION"
+    RED = "DANGER"
+    
+class StopCondition(Enum):
+    WAITING = "Waiting autorization"
+    AUTORIZED = "Autorized stop"
+    DENIED = "Denied stop"
+    EMERGENCY = "Emergency post"
+    STOPPED = "Stopped"
+    
+@dataclass
+class Module: 
+    """
+    Represents a module to Mars
+    """
+    
+    id: int
+    type: ModuleType
+    weight: float
+    arrival_time: float
+    stop_condition: StopCondition = StopCondition.WAITING
+    
+    def __str__(self):
+        return f"Module {self.id} - Type: {self.type.value}, Weight: {self.weight}kg, Arrival: {time.ctime(self.arrival_time)}, Condition: {self.stop_condition.value}"
+    
+    def __lt__(self, other):
+        """Defines the less-than operator for sorting modules by arrival time."""
+        return self.arrival_time < other.arrival_time
+    
+# === Algorithms for searching and sorting === 
 
+class SearchAlgorithms:
+    
+    @staticmethod
+    def linear_search():
+        pass
+    
+    @staticmethod
+    def binary_search():
+        pass
+    
+    
 # =============================================================================
 # [2] ESTRUTURAS LINEARES — Operações de fila, pilha e verificação
 # =============================================================================
@@ -57,6 +111,3 @@ import math
 # =============================================================================
 # Ponto de entrada
 # =============================================================================
-
-if __name__ == "__main__":
-    main()
